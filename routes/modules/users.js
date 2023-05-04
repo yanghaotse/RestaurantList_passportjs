eaf4b4b1a6957d26152ef0a00bbee4878576f41c
yang haotse <ha91252@gmail.com>
2023-05-04 18:24:50 +0800

    feat: add user model

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,8 +5,8 @@ router.get('/login',(req, res) => {
   res.render('login')
 }) 
 
-// router.get('/register', (req, res) => {
-
-// })
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 module.exports = router
