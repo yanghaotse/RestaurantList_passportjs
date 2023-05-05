81bd5ad28a68959786ab39c71b39fcbc12321b08
yang haotse <ha91252@gmail.com>
2023-05-05 11:44:13 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
+const session = require('express-session') // 引入session
 const exphbs = require('express-handlebars')
-
 const Restaurant = require('./models/restaurant')
 const bodyParser = require("body-parser")
 const methodOverride = require('method-override')
@@ -12,6 +12,16 @@ const port = 3000
 
 app.engine('hbs', exphbs({defaultLayout : 'main', extname : '.hbs'}))
 app.set('view engine', 'hbs')
+
+// 設定session
+app.use(session({
+  secret:'ThisIsMySecret',
+  resave: false, 
+  // resave: 設定為 true 時，會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+  saveUninitialized: true
+  // saveUninitialized: 強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session
+}))
+
 app.use(express.urlencoded({ extended : true })) //body-parser
 app.use(express.static('public')) //使用靜態檔案
 
