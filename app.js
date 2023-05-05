1233d01404c0ed2f6473e9328abe3709da860de2
yang haotse <ha91252@gmail.com>
2023-05-05 12:43:43 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser")
 const methodOverride = require('method-override')
 const routes = require('./routes')//引入路由器時，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案
 require('./config/mongoose')
+const usePassport = require('./config/passport')
 
 const app = express()
 const port = 3000
@@ -24,8 +25,8 @@ app.use(session({
 
 app.use(express.urlencoded({ extended : true })) //body-parser
 app.use(express.static('public')) //使用靜態檔案
-
 app.use(methodOverride('_method'))
+usePassport(app)
 app.use(routes)
 
 app.listen( port, () => {
