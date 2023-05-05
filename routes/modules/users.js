13c8585ac4fe6feb9683ac306c6920fe8dc9d30a
yang haotse <ha91252@gmail.com>
2023-05-05 12:48:25 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -35,5 +35,9 @@ router.post('/register',(req, res) => {
   })
   .catch(err => console.log(err))
 })
+router.get('/logout', (req, res) => {
+  req.logout() //Passport.js 提供的函式
+  res.redirect('/users/login')
+})
 
 module.exports = router
